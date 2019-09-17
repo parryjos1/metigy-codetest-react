27f96680c6f49d85277facc3c7dbc7afd8924a28
Josh Parry <josh.parry97@gmail.com>
2019-09-17 17:24:54 +1000

    Updates

---
+++ precache-manifest.8e9e3dd5cc54acf5b5067f4640ef945f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332bab6bdb5dfd218deb9e8dcb9e56d0",
+    "url": "/metigy-codetest-react/index.html"
+  },
+  {
+    "revision": "d97e10afd1defe9c0056",
+    "url": "/metigy-codetest-react/static/css/main.ecf28771.chunk.css"
+  },
+  {
+    "revision": "124c8e77ba923d098406",
+    "url": "/metigy-codetest-react/static/js/2.fcc59dfc.chunk.js"
+  },
+  {
+    "revision": "d97e10afd1defe9c0056",
+    "url": "/metigy-codetest-react/static/js/main.b5595f4b.chunk.js"
+  },
+  {
+    "revision": "b90f8d8434c2f622c49b",
+    "url": "/metigy-codetest-react/static/js/runtime~main.08bffb6a.js"
+  }
+]);
